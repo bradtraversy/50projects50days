6b33e865383bfe2b7b2a34dd0faa6b35641bfa60
Brad Traversy <bradtraversy@local.local>
2020-10-13 12:10:44 -0400

    FAQ collapse

---
+++ faq-collapse/script.js
@@ -0,0 +1,7 @@
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
