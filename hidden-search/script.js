a083b6583f42d6393cf142806470a1b3b6baabda
Brad Traversy <bradtraversy@local.local>
2020-10-15 15:18:45 -0400

    Hidden search widget

---
+++ hidden-search/script.js
@@ -0,0 +1,8 @@
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
