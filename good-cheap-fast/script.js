601a5ef151482ee52ec3db4ce81d2051c3faa2de
Brad Traversy <bradtraversy@local.local>
2020-10-15 11:23:13 -0400

    Good, cheap, fast checkboxes

---
+++ good-cheap-fast/script.js
@@ -0,0 +1,22 @@
+const toggles = document.querySelectorAll('.toggle')
+const good = document.querySelector('#good')
+const cheap = document.querySelector('#cheap')
+const fast = document.querySelector('#fast')
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
+
+function doTheTrick(theClickedOne) {
+    if(good.checked && cheap.checked && fast.checked) {
+        if(good === theClickedOne) {
+            fast.checked = false
+        }
+
+        if(cheap === theClickedOne) {
+            good.checked = false
+        }
+
+        if(fast === theClickedOne) {
+            cheap.checked = false
+        }
+    }
+}
