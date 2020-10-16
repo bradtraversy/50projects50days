adcd5f26b56ac5124ebbd8a545800eb93e8522a2
Brad Traversy <bradtraversy@local.local>
2020-10-16 13:24:53 -0400

    Incrementing counter

---
+++ incrementing-counter/script.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+
+        const increment = target / 200
+
+        if(c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
+        }
+    }
+
+    updateCounter()
+})
