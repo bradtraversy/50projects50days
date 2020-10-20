d82e9010e158777b0edcc7ffb67d3395f29ebcfe
Brad Traversy <bradtraversy@local.local>
2020-10-20 13:20:03 -0400

    Random image generator

---
+++ random-image-generator/script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('.container')
+const unsplashURL = 'https://source.unsplash.com/random/'
+const rows = 5
+
+for(let i = 0; i < rows * 3; i++) {
+    const img = document.createElement('img')
+    img.src = `${unsplashURL}${getRandomSize()}`
+    container.appendChild(img)
+}
+
+function getRandomSize() {
+    return `${getRandomNr()}x${getRandomNr()}`
+}
+
+function getRandomNr() {
+    return Math.floor(Math.random() * 10) + 300
+}
