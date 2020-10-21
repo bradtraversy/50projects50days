01ae675ae57c7d9692ccb5858222abcf29ebddc5
Brad Traversy <bradtraversy@local.local>
2020-10-21 11:11:34 -0400

    Auto text effect

---
+++ auto-text-effect/script.js
@@ -0,0 +1,22 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'We Love Programming!'
+let idx = 1
+let speed = 300 / speedEl.value
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if(idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+}
+
+
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
