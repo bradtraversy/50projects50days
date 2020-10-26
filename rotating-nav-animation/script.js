2902b2e2dca979fd2bdd69be2af668327a7c8a20
Brad Traversy <bradtraversy@local.local>
2020-10-26 14:15:19 -0400

    Rotating nav animation

---
+++ rotating-nav-animation/script.js
@@ -0,0 +1,7 @@
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+
+open.addEventListener('click', () => container.classList.add('show-nav'))
+
+close.addEventListener('click', () => container.classList.remove('show-nav'))
