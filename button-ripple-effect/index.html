fb5469b845bb18c06c170e0e1df2264926efbbfc
Brad Traversy <bradtraversy@local.local>
2020-10-26 10:40:00 -0400

    Button ripple effect

---
+++ button-ripple-effect/index.html
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Button Ripple Effect</title>
+  </head>
+  <body>
+    <button class="ripple">Click Me</button>
+    <script src="script.js"></script>
+  </body>
+</html>
---
+++ button-ripple-effect/script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('.ripple')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.clientX
+        const y = e.clientY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 500)
+    })
+})
