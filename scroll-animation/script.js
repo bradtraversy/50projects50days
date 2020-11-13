f115c8031d1d3a976670b905c5e86ec361cb85e6
Brad Traversy <bradtraversy@local.local>
2020-11-13 10:23:00 -0500

    Scroll animation

---
+++ scroll-animation/script.js
@@ -0,0 +1,19 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
