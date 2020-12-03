f45bff61f253be71a426272c27f36a13b6111149
Ahete007 <35165131+Ahete007@users.noreply.github.com>
2020-12-03 22:18:43 +0530

    Update script.js
    we can use toggle here instead of calling a separate function for removing the class.

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,14 +1,9 @@
-const panels = document.querySelectorAll('.panel')
+const panels=document.querySelectorAll('.panel')
+panels.forEach(panel=>{
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
+    panel.addEventListener('click',()=>{
+
+        panel.classList.toggle('active')
     })
 })
 
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
-}
