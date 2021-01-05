d24b8bd9cf6f68dc94bf25b20ec29d7b56d9dc0e
yolo <1084677348@qq.com>
2021-01-05 16:05:06 +0800

    Revert "fix(verify-account-ui): Cannot read property 'focus' of undefined"
    This reverts commit cb45acb2e249c42b196b2ceb7f98cb009cb0a997.

--- verify-account-ui/script.js
+++ verify-account-ui/script.js
@@ -1,5 +1,4 @@
 const codes = document.querySelectorAll('.code')
-const length = codes.length
 
 codes[0].focus()
 
@@ -7,14 +6,8 @@ codes.forEach((code, idx) => {
     code.addEventListener('keydown', (e) => {
         if(e.key >= 0 && e.key <=9) {
             codes[idx].value = ''
-            if (length - 1 == idx) {
-              return  
-            }
             setTimeout(() => codes[idx + 1].focus(), 10)
         } else if(e.key === 'Backspace') {
-            if (idx == 0) {
-              return  
-            }
             setTimeout(() => codes[idx - 1].focus(), 10)
         }
     })
