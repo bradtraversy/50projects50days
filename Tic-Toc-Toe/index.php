03fcf44e66be4a534b01f41fff0014b38a7afa57
Mazhar Hussain <maxhar30@gmail.com>
2021-05-20 20:25:17 +0500

    Message Added

--- Tic-Toc-Toe/index.php
+++ Tic-Toc-Toe/index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tic Tac Toe</title>
 
-    <link rel="stylesheet" href="styles.css"> 
+    <link rel="stylesheet" href="styles.css">
     <script src="script.js" defer></script>
 </head>
+
 <body>
     <div class="board" id="board">
         <div class="cell" data-cell></div>
@@ -19,10 +21,15 @@
         <div class="cell" data-cell></div>
         <div class="cell" data-cell></div>
         <div class="cell" data-cell></div>
-      </div>
-      <div class="winning-message" id="winningMessage">
+    </div>
+    <div class="winning-message" id="winningMessage">
         <div data-winning-message-text></div>
         <button id="restartButton">Restart</button>
-      </div>
+    </div>
+
+    <center>
+        <h1>Coded By <a href="https://github.com/Maxhar30">Mazhar Hussain</a></h1>
+    </center>
 </body>
+
 </html>
