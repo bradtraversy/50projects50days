58bfc2444e05c0301005dbe800f8d59d8204b21d
SHUBHAM KUMAR VISHNOI <65655892+shubhamvishnoi3288@users.noreply.github.com>
2021-10-06 19:40:34 +0530

    Update script.js

--- faq-collapse/script.js
+++ faq-collapse/script.js
@@ -3,5 +3,6 @@ const toggles = document.querySelectorAll('.faq-toggle')
 toggles.forEach(toggle => {
     toggle.addEventListener('click', () => {
         toggle.parentNode.classList.toggle('active')
+        //   toggle.parentNode.classList.toggle('inactive')
     })
 })
