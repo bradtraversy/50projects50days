5a280ce2847fe45960dba76e8d6e76c6b5027098
couldntfindabetterusername <bt20ece098@iiitn.ac.in>
2022-03-09 10:02:23 +0530

    day 23 completed

--- my code/23-kinetic-loader/script.js
+++ my code/23-kinetic-loader/script.js
@@ -1,8 +1,8 @@
 const one = document.getElementById("one");
 const two = document.getElementById("two");
 
-while (1) {
-  setTimeout(() => {
-    one.classList.add();
-  }, 2000);
-}
+// while (1) {
+//   setTimeout(() => {
+//     one.classList.add();
+//   }, 2000);
+// }
