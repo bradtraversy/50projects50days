d2fc33eeb90bacacc4ca698bbe4bc7aa7c21d3d5
omaranBazna <omran4d@gmail.com>
2022-06-06 23:35:01 -0400

    add log history to the event keycode

--- event-keycodes/script.js
+++ event-keycodes/script.js
@@ -1,6 +1,25 @@
 const insert = document.getElementById('insert')
-
+const log = document.getElementById('log')
+let lastEvent=null;
 window.addEventListener('keydown', (event) => {
+if(lastEvent){
+  log.innerHTML +=`<div>
+     <div class="key">
+     ${lastEvent.key === ' ' ? 'Space' : lastEvent.key} 
+     <small>event.key</small>
+     </div>
+    <div class="key">
+  ${lastEvent.keyCode}
+  <small>event.keyCode</small>
+</div>
+
+<div class="key">
+  ${lastEvent.code}
+  <small>event.code</small>
+</div>
+</div>
+  `
+}
   insert.innerHTML = `
   <div class="key">
   ${event.key === ' ' ? 'Space' : event.key} 
@@ -16,5 +35,7 @@ window.addEventListener('keydown', (event) => {
   ${event.code}
   <small>event.code</small>
 </div>
+
   `
+  lastEvent=event;
 })
