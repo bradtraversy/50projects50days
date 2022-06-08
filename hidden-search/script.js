7aafbefc2b18294153ccbfa922693bb4f7d07b49
omaranBazna <omran4d@gmail.com>
2022-06-07 21:59:52 -0400

    hide the search bar when the user don't do anything for ten seconds

--- hidden-search/script.js
+++ hidden-search/script.js
@@ -1,8 +1,20 @@
 const search = document.querySelector('.search')
 const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
-
+const time_limit=10000;
+let active=false;
 btn.addEventListener('click', () => {
     search.classList.toggle('active')
     input.focus()
+    active =!active
+    if(active){
+   var time_out= setTimeout(()=>{
+        search.classList.toggle('active')
+    },time_limit)
+}
+})
+input.addEventListener('change',()=>{
+    active=true;
+    
+    clearTimeout(time_out);
 })
