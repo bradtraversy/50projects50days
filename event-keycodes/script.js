dfc862488a90d3773034ab829b3cb93905f040f7
Hosseinghafouri <hossenghafouri@gmail.com>
2022-08-21 19:20:12 +0430

    Update and add more features
    Change in all three files html, css, js
    * Change in the design model (improvement and optimization)
    * Add more features
    ... Etc

--- event-keycodes/script.js
+++ event-keycodes/script.js
@@ -1,20 +1,25 @@
-const insert = document.getElementById('insert')
+let $ = document;
+let title = $.querySelector('title');
+let keyCode = $.querySelector('#keyCode');
+let eventKey = $.querySelector('#key');
+let eventLocation = $.querySelector('#location');
+let eventWhich = $.querySelector('#which');
+let eventCode = $.querySelector('#code');
 
-window.addEventListener('keydown', (event) => {
-  insert.innerHTML = `
-  <div class="key">
-  ${event.key === ' ' ? 'Space' : event.key} 
-  <small>event.key</small>
-</div>
-
-<div class="key">
-  ${event.keyCode}
-  <small>event.keyCode</small>
-</div>
-
-<div class="key">
-  ${event.code}
-  <small>event.code</small>
-</div>
-  `
-})
+$.body.addEventListener('keydown', (event) => {
+	event.preventDefault();
+	starter.style.display = 'none';
+	heading.style.display = 'flex';
+	ascii.style.display = 'flex';
+	infos.style.display = 'flex';
+	let getKey = event.key;
+	let getLocation = event.location;
+	let getWich = event.which;
+	let getCode = event.code;
+	title.innerHTML = "key codes : " + getKey;
+	keyCode.innerHTML = getKey;
+	eventKey.innerHTML = getKey;
+	eventLocation.innerHTML = getLocation;
+	eventWhich.innerHTML = getWich;
+	eventCode.innerHTML = getCode;
+});
