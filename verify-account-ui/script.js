534a6781a386f476b4d59947916fa0069402f73a
Kaiyang Yao <yaoky2008@gmail.com>
2023-01-22 00:28:21 -0600

    prevent error caused by index out of bound

--- verify-account-ui/script.js
+++ verify-account-ui/script.js
@@ -6,9 +6,9 @@ codes.forEach((code, idx) => {
     code.addEventListener('keydown', (e) => {
         if(e.key >= 0 && e.key <=9) {
             codes[idx].value = ''
-            setTimeout(() => codes[idx + 1].focus(), 10)
+            setTimeout(() => codes[idx < codes.length - 1 ? idx + 1 : idx].focus(), 10)
         } else if(e.key === 'Backspace') {
-            setTimeout(() => codes[idx - 1].focus(), 10)
+            setTimeout(() => codes[idx > 0 ? idx - 1 : idx].focus(), 10)
         }
     })
 })
