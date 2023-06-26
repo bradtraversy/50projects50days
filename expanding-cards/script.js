6f8eb5093986721699ca3762aa2886f0dccfa438
4change <e_mail4gxf@163.com>
2023-06-26 16:10:43 +0800

    Expanding Cards——添加注释

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,5 +1,7 @@
+// 获取所有类选择器panel选中的元素
 const panels = document.querySelectorAll('.panel')
 
+// 为每个panel元素注册事件监听器——在触发点击事件时，移除所有panel的active类属性，并为当前panel添加active类属性
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
