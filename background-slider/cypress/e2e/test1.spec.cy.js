eef17cd5c6befa28904f670d566b74d140ce2900
Hammad1007 <hammadrashid2001@gmail.com>
2023-12-20 16:57:11 +0500

    Update the test

--- background-slider/cypress/e2e/test1.spec.cy.js
+++ background-slider/cypress/e2e/test1.spec.cy.js
@@ -1,5 +1,50 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Project 4: Background Slider', () => {
+  
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/background-slider')
+  })
+
+  it('Opens the home page', () => {
+    
+  })
+
+  it('Screen is visible', () => {
+    cy.get('body').should('be.visible')
+  })
+
+  context('Image Slider', () => {
+    it('Slider container is visible', () => {
+      cy.get('.slider-container').should('be.visible')
+    })
+
+   context('Arrows', () => {
+    it('Arrows are visible', () => {
+      cy.get('.arrow').should('be.visible')
+    })
+
+    it('Color of arrows is white', () => {
+      cy.get('.arrow').should('have.css', 'color', 'rgb(255, 255, 255)')
+    })
+
+    context('Left Arrow', () => {
+      it('Left arrow is visible', () => {
+        cy.get('.fa-arrow-left ').should('be.visible')
+      })
+
+      it('Left arrow is clickable', () => {
+        cy.get('.fa-arrow-left ').should('be.visible').click()
+      })
+    })
+
+    context('Right Arrow', () => {
+      it('Right arrow is visible', () => {
+        cy.get('.fa-arrow-right ').should('be.visible')
+      })
+
+      it('Right arrow is clickable', () => {
+        cy.get('.fa-arrow-right ').should('be.visible').click()
+      })
+    })
+   })
   })
 })
