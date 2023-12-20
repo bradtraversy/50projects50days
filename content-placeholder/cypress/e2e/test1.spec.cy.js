d0d67e3f28e858f5337a52f7c4285b0f887ad780
Hammad1007 <hammadrashid2001@gmail.com>
2023-12-20 18:38:32 +0500

    Update the test

--- content-placeholder/cypress/e2e/test1.spec.cy.js
+++ content-placeholder/cypress/e2e/test1.spec.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Project 6: Content Placeholder', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/content-placeholder')
+  })
+
+  it('Opens the home page', () => {
+
+  })
+
+  it('Background color', () => {
+
   })
 })
