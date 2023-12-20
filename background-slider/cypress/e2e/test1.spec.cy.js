29ddfa0b33b29fc09bcb0825abed0544f11624a8
Hammad1007 <hammadrashid2001@gmail.com>
2023-12-20 17:24:17 +0500

    Update test file

--- background-slider/cypress/e2e/test1.spec.cy.js
+++ background-slider/cypress/e2e/test1.spec.cy.js
@@ -39,6 +39,7 @@ describe('Project 4: Background Slider', () => {
         cy.get('.fa-arrow-left ').should('be.visible').click()
         cy.get('.slide').should('have.class', 'active').should('be.visible')
       })
+
     })
 
     context('Right Arrow', () => {
@@ -54,7 +55,35 @@ describe('Project 4: Background Slider', () => {
         cy.get('.fa-arrow-right ').should('be.visible').click()
         cy.get('.slide').should('have.class', 'active').should('be.visible')
       })
+
     })
    })
+  })
+
+  context('Background Image', () => {
+    it('Verifies the background image of the first slide', () => {
+      cy.get('.slide').first().should('have.css', 'background-image')
+        .and('include', 'photo-1549880338-65ddcdfd017b')
+    })
+
+    it('Verifies the background image of the second slide', () => {
+      cy.get('.slide').eq(1).should('have.css', 'background-image')
+        .and('include', 'photo-1511593358241-7eea1f3c84e5')
+    })
+
+    it('Verifies the background image of the third slide', () => {
+      cy.get('.slide').eq(2).should('have.css', 'background-image')
+        .and('include', 'photo-1495467033336-2effd8753d51')
+    })
+
+    it('Verifies the background image of the fourth slide', () => {
+      cy.get('.slide').eq(3).should('have.css', 'background-image')
+        .and('include', 'photo-1522735338363-cc7313be0ae0')
+    })
+
+    it('Verifies the background image of the fifth slide', () => {
+      cy.get('.slide').last().should('have.css', 'background-image')
+        .and('include', 'photo-1559087867-ce4c91325525')
+    })
   })
 })
