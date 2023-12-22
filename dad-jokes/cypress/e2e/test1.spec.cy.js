ab91c8b13c145c1785380fb28a03b181a90beec7
Hammad1007 <hammadrashid2001@gmail.com>
2023-12-22 11:06:54 +0500

    Update testing code

--- dad-jokes/cypress/e2e/test1.spec.cy.js
+++ dad-jokes/cypress/e2e/test1.spec.cy.js
@@ -1,5 +1,31 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Project: Dad Jokes', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/dad-jokes/index.html')
+  })
+
+  context('General Tests', () => {
+    it('Opens the home page', () => {
+
+    })
+
+    it('Screen is visible', () => {
+      cy.get('body').should('be.visible')
+    })
+
+    it('Background color', () => {
+      cy.get('body').should('have.css', 'background-color', 'rgb(104, 109, 224)')
+    })
+  })
+
+  context('Body', () => {
+    context('Title Text', () => {
+      it('Title text is visible', () => {
+        cy.get('body').get('.container h3').should('be.visible')
+      })
+
+      it('Title text is correct', () => {
+        cy.get('body').get('.container h3').should('have.text', "Don't Laugh Challenge")
+      })
+    })
   })
 })
