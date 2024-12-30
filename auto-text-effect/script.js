c9c7b4f9ede3eecac5d741735b9beea98e7a80ca
Pranav-Nikhil <pranavnikhil005@gmail.com>
2024-12-30 16:58:05 +0530

    auto-text-effect

--- auto-text-effect/script.js
+++ auto-text-effect/script.js
@@ -1,6 +1,6 @@
 const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
-const text = 'We Love Programming!'
+const text = 'Hello Have a swanky Day'
 let idx = 1
 let speed = 300 / speedEl.value
 
